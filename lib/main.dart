092fb2d1719c65c02c338fb685f4ad70e770c007
Bellapasorong <86918789+Bellapasorong@users.noreply.github.com>
2022-06-01 02:51:23 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:postest7_1915016151_hilarybella/splash_screen.dart';
+import 'package:get/get.dart';
+ 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
